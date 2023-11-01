7ca5f7ab38a4608a5f1d7cf2546f477faad562f5
muriloPereira006 <muriloedu006@gmail.com>
2023-10-31 22:40:42 -0300

    draft:input squema

--- src/components/inputGeneric/index.jsx
+++ src/components/inputGeneric/index.jsx
@@ -1,16 +1,68 @@
 import PropTypes from "prop-types"
 import { ReactComponent as ToolTipIconClosed } from "../../assets/icons/tootipClosed.svg"
 import { ReactComponent as ToolTipIcon } from "../../assets/icons/tootip.svg"
-import { } from "./styles"
+import { Container, InputFild, IconContainer } from "./styles"
 // import Colors from "../../assets/colors"
 // import { FontFamily, FontSize, FontWeight, FontStyles } from "../../assets/typography"
 
 const InputGeneric = ({
     type = 'text',
 }) => {
-    return (
-        <>{type}</>
-    );
+    switch (type) {
+        case 'email':
+            return (
+                <Container>
+                    <InputFild />
+                </Container>
+            );
+        case 'password':
+            return (
+                <Container>
+                    <InputFild />
+                </Container>
+            );
+
+        case 'cep':
+            return (
+                <Container>
+                    <InputFild />
+                </Container>
+            );
+
+        case 'cpf':
+            return (
+                <Container>
+                    <InputFild />
+                </Container>
+            );
+
+        case 'phone':
+            return (
+                <Container>
+                    <InputFild />
+                </Container>
+            );
+
+        case 'date':
+            return (
+                <Container>
+                    <InputFild />
+                </Container>
+            );
+
+        default:
+            return (
+                <Container>
+                    <InputFild />
+                    <IconContainer>
+                        <ToolTipIcon />
+                    </IconContainer>
+                    <IconContainer>
+                        <ToolTipIconClosed />
+                    </IconContainer>
+                </Container>
+            );
+    }
 };
 
 export default InputGeneric;
